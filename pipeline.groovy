7db6325050226427bfdc8402d8e1bda7e863e2bd
hamza-t <Hamza.rouaissi@insat.u-carthage.tn>
2022-12-09 02:08:54 -0800

    sonarqube fix config

--- pipeline.groovy
+++ pipeline.groovy
@@ -13,11 +13,4 @@ def runUnitTests(){
 def sonarQubeTest(){
 	sh "mvn sonar:sonar -Dsonar.login=admin -Dsonar.password=devops"
 }
-def pushToNexus() {
-    echo 'pushing to nexus...'
-    sh 'mvn clean package -Dmaven.test.skip=true deploy:deploy-file -DgroupId=com.example -DartifactId=devops-project -Dversion=1.0 -DgeneratePom=true -Dpackaging=jar -DrepositoryId=deploymentRepo -Durl=http://localhost:8081/repository/maven-releases/ -Dfile=target/devops-project-0.0.1-SNAPSHOT.jar'
-} 
-def dockerBuild(){
-	sh "docker build -t dockerdevops ."
-}
 return this
